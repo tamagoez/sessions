cee696b9e4e40cdf2492a5d85f5fea7a358f22aa
tamagoes <87574268+tamagoez@users.noreply.github.com>
2021-10-27 20:59:35 +0900

    Change GUI

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import { supabase } from 'lib/Store'
+import { useState } from "react";
+import { supabase } from "lib/Store";
 
 const Home = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = async (type, username, password) => {
     try {
       const { error, user } =
-        type === 'LOGIN'
+        type === "LOGIN"
           ? await supabase.auth.signIn({ email: username, password })
-          : await supabase.auth.signUp({ email: username, password })
+          : await supabase.auth.signUp({ email: username, password });
       // If the user doesn't exist here and an error hasn't been raised yet,
       // that must mean that a confirmation email has been sent.
       // NOTE: Confirming your email address is required by default.
       if (error) {
-        alert('Error with auth: ' + error.message)
-      } else if (!user) alert('Signup successful, confirmation mail should be sent soon!')
+        alert("Error with auth: " + error.message);
+      } else if (!user)
+        alert("Signup successful, confirmation mail should be sent soon!");
     } catch (error) {
-      console.log('error', error)
-      alert(error.error_description || error)
+      console.log("error", error);
+      alert(error.error_description || error);
     }
-  }
+  };
 
   return (
     <div className="w-full h-full flex justify-center items-center p-4 bg-gray-300">
       <div className="w-full sm:w-1/2 xl:w-1/3">
         <div className="border-teal p-8 border-t-12 bg-white mb-6 rounded-lg shadow-lg bg-white">
           <div className="mb-4">
-            <label className="font-bold text-grey-darker block mb-2">Email</label>
+            <label className="font-bold text-grey-darker block mb-2">
+              Username
+            </label>
             <input
               type="text"
               className="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow"
@@ -38,7 +41,9 @@ const Home = () => {
             />
           </div>
           <div className="mb-4">
-            <label className="font-bold text-grey-darker block mb-2">Password</label>
+            <label className="font-bold text-grey-darker block mb-2">
+              Password
+            </label>
             <input
               type="password"
               className="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow"
@@ -51,29 +56,29 @@ const Home = () => {
           <div className="flex flex-col gap-2">
             <a
               onClick={(e) => {
-                e.preventDefault()
-                handleLogin('SIGNUP', username, password)
+                e.preventDefault();
+                handleLogin("LOGIN", username, password);
               }}
-              href={'/channels'}
-              className="bg-green-500 hover:bg-teal text-white py-2 px-4 rounded text-center transition duration-150 hover:bg-indigo-600 hover:text-white"
+              href={"/channels"}
+              className="bg-green-500 hover:bg-teal text-white py-2 px-4 rounded text-center transition duration-150 hover:bg-white hover:text-green-500 border hover:border-green-500"
             >
-              Sign up
+              Login
             </a>
             <a
               onClick={(e) => {
-                e.preventDefault()
-                handleLogin('LOGIN', username, password)
+                e.preventDefault();
+                handleLogin("SIGNUP", username, password);
               }}
-              href={'/channels'}
-              className="border border-indigo-700 text-indigo-700 py-2 px-4 rounded w-full text-center transition duration-150 hover:bg-indigo-700 hover:text-white"
+              href={"/channels"}
+              className="border border-green-500 text-green-500 py-2 px-4 rounded w-full text-center transition duration-150 hover:bg-green-500 hover:text-white"
             >
-              Login
+              Sign up
             </a>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

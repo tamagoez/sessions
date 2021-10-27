9fd008d319dbe84cda223fb8a0fe9559c331d2fd
tamagoes <87574268+tamagoez@users.noreply.github.com>
2021-10-27 21:02:15 +0900

    Change GUI

--- components/Layout.js
+++ components/Layout.js
@@ -29,13 +29,13 @@ export default function Layout(props) {
     <main className="main flex h-screen w-screen overflow-hidden">
       {/* Sidebar */}
       <nav
-        className="w-64 bg-gray-900 text-gray-100 overflow-scroll "
+        className="w-64 bg-green-900 text-gray-100 overflow-scroll "
         style={{ maxWidth: '20%', minWidth: 150, maxHeight: '100vh' }}
       >
         <div className="p-2 ">
           <div className="p-2">
             <button
-              className="bg-blue-900 hover:bg-blue-800 text-white py-2 px-4 rounded w-full transition duration-150"
+              className="bg-gleen-900 hover:bg-gleen-800 text-white py-2 px-4 rounded w-full transition duration-150"
               onClick={() => newChannel()}
             >
               New Channel
@@ -45,7 +45,7 @@ export default function Layout(props) {
           <div className="p-2 flex flex-col space-y-2">
             <h6 className="text-xs">{user?.email}</h6>
             <button
-              className="bg-blue-900 hover:bg-blue-800 text-white py-2 px-4 rounded w-full transition duration-150"
+              className="bg-green-900 hover:bg-blue-800 text-white py-2 px-4 rounded w-full transition duration-150"
               onClick={() => signOut()}
             >
               Log out

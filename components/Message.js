3a2d772fcb7114efe07937f5af50d5bd7bd05da6
tamagoes <87574268+tamagoez@users.noreply.github.com>
2021-11-03 20:11:40 +0900

    Change for adding anchor

--- components/Message.js
+++ components/Message.js
@@ -7,6 +7,16 @@ const Message = ({ message }) => {
   const { user, userRoles } = useContext(UserContext)
   const replacedmessage = message.inserted_at.replace('T', '  ').replace('-', '/').replace('-', '/').replace('Z', '')
 
+  function addid(textstr){
+    if (!(textstr.indexOf(">>", 0) == -1)) {
+      console.log('>> found!')
+      return textstr
+    } else {
+      console.log('>> Not found!')
+      return textstr
+    }
+  }
+
   return (
     <div className="py-1 flex items-center space-x-2">
       <div className="text-gray-100 w-4">
@@ -20,7 +30,7 @@ const Message = ({ message }) => {
       <div>
         <a name={message.id}><p className="text-white font-bold">{message.author.username}</p>
         <p className="text-gray-500">{message.id} - {replacedmessage.substring(0, replacedmessage.indexOf("."))}</p>
-        <p className="text-white">{message.message}</p></a>
+        <p className="text-white">{addid(message.message)}</p></a>
       </div>
     </div>
   )

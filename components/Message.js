de9461378b8d72a111f8c4a77ba7b8a0616d6f28
tamagoes <87574268+tamagoez@users.noreply.github.com>
2021-11-03 21:20:11 +0900

    Change bug for temp

--- components/Message.js
+++ components/Message.js
@@ -38,8 +38,7 @@ const Message = ({ message }) => {
       <div>
         <a name={message.id}><p className="text-white font-bold">{message.author.username}</p>
         <p className="text-gray-500">{message.id} - {replacedmessage.substring(0, replacedmessage.indexOf("."))}</p>
-        <div id={message.id}><p className="text-white"></p></div></a>
-        {changedmess(message.message, message.id)}
+        <div id={message.id}><p className="text-white"></p>{addid(message.message, message.id).replace('%%', '>>')}</div></a>
       </div>
     </div>
   )

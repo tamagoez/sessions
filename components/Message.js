b3a6e9219280663a87d185dd5f4920e245fc0e28
tamagoes <87574268+tamagoez@users.noreply.github.com>
2021-11-30 21:40:59 +0900

    Test again

--- components/Message.js
+++ components/Message.js
@@ -38,7 +38,7 @@ const Message = ({ message }) => {
       <div>
         <a name={message.id}><p className="text-white font-bold">{message.author.username}</p>
         <p className="text-gray-500">{message.id} - {replacedmessage.substring(0, replacedmessage.indexOf("."))}</p>
-        <p className="text-white" id={message.id}><a href="#{message.id}">{message.message}</a></p></a>
+        <p className="text-white" id={message.id}><a href="#163">{message.message}</a></p></a>
       </div>
     </div>
   )

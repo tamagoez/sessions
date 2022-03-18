561bae4707b20096ae9618b067408604ca3a2711
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-03-18 21:37:03 +0900

    Update index.js

--- pages/settings/index.js
+++ pages/settings/index.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { supabase } from '~/lib/supabaseClient'
+import { supabase } from '~/utils/supabaseClient'
 import AvatarSetting from '~/components/Avatar'
 import { useRouter } from 'next/router'
 

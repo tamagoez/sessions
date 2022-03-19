67e82a268d54be545ad7562460805ba97221f6cc
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-03-19 11:01:12 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,9 +4,10 @@ import React, { useState, useEffect } from 'react'
 import Router from 'next/router'
 import UserContext from 'lib/UserContext'
 import { supabase, fetchUserRoles } from 'lib/Store'
-
+import App from 'next/app'
 
 export default function SupabaseSlackClone({ Component, pageProps }) {
+  App.getInitialProps = async () => ({ pageProps: {} })
   const [userLoaded, setUserLoaded] = useState(false)
   const [user, setUser] = useState(null)
   const [session, setSession] = useState(null)

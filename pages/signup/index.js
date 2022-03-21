157386bfd2934af85144a8d2326bbfd9a339e47c
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-03-21 22:06:22 +0900

    give next props each other

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -104,7 +104,7 @@ const Home = () => {
               Sign up
             </a>
             <a
-              href={"../login"}
+              href={"/login?next=" + nextlink}
               className="bg-green-400 hover:bg-teal text-white py-1 px-4 rounded text-center transition duration-150 hover:bg-white hover:text-green-400 border hover:border-green-400"
             >
               or Login

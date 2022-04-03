b5b8f40d909264f816a4dcf4918afe6f1357b774
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-04-03 10:15:47 +0900

    Update MessageInput.js

--- components/MessageInput.js
+++ components/MessageInput.js
@@ -14,7 +14,7 @@ const MessageInput = ({ onSubmit }) => {
   return (
     <>
       <input
-        className="bg-white border border-solid border-gray-300 appearance-none rounded w-full py-2 px-3 text-black leading-tight focus:shadow-outline"
+        className="bg-base-100 border border-solid border-gray-300 appearance-none rounded w-full py-2 px-3 text-black-800 leading-tight focus:shadow-outline"
         type="text"
         placeholder="Send a message"
         value={messageText}

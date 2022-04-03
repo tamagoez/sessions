07874f7a44d234f782304c65799dc83f1007e7a6
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-04-03 11:52:30 +0900

    Update Message.js

--- components/Message.js
+++ components/Message.js
@@ -25,25 +25,7 @@ const Message = ({ message }) => {
     return urlgot;
   }
   
-  const shimmer = (w, h) => `
-<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <defs>
-    <linearGradient id="g">
-      <stop stop-color="#333" offset="20%" />
-      <stop stop-color="#222" offset="50%" />
-      <stop stop-color="#333" offset="70%" />
-    </linearGradient>
-  </defs>
-  <rect width="${w}" height="${h}" fill="#333" />
-  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-</svg>`
-
-const toBase64 = (str) =>
-  typeof window === 'undefined'
-    ? Buffer.from(str).toString('base64')
-    : window.btoa(str)
-
+  
   function replacetz(time) {
     // const systemtz = DateTime.now().locale;
     // Settings.defaultZone = "system";
@@ -59,7 +41,6 @@ const toBase64 = (str) =>
               src={getURL(id)}
               alt={"Avatar of " + username}
               placeholder="blur"
-              blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
               width={100}
               height={100}
             />

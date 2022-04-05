939c85259ee6c366664be92599a774e85f357e01
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-04-06 02:50:04 +0900

    Ver. 0.4.2
    Add `target="_blank" rel="noopener noreferrer"` to fix bug(caused by iframe)

--- components/Message.js
+++ components/Message.js
@@ -158,7 +158,7 @@ const Message = ({ message }) => {
           }
         >
           <p className="text-black-900 m-2">
-            <ReactMarkdown remarkPlugins={[gfm]} unwrapDisallowed={false}>
+            <ReactMarkdown remarkPlugins={[gfm]} unwrapDisallowed={false} renderers={{link: props => <a href={props.href} target="_blank" rel="noopener noreferrer">{props.children}</a>}}>
               {message.message}
             </ReactMarkdown>
           </p>

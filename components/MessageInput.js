43e4f381f2f2045c3e0ccd501ae2131ae1e7eaea
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-04-06 21:55:24 +0900

    Update MessageInput.js

--- components/MessageInput.js
+++ components/MessageInput.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 const MessageInput = ({ onSubmit }) => {
   const [messageText, setMessageText] = useState("");
+  const [uploading, setUploading] = useState(false)
 
   const submitOnEnter = (event) => {
     // Watch for enter key
@@ -10,9 +11,54 @@ const MessageInput = ({ onSubmit }) => {
       setMessageText("");
     }
   };
+  
+  async function uploadFile(event) {
+    try {
+      setUploading(true)
+
+      if (!event.target.files || event.target.files.length === 0) {
+        throw new Error('You must select a file to upload.')
+      }
+
+      const file = event.target.files[0]
+      const fileExt = file.name.split('.').pop()
+      const fileName = `${Math.random()}.${fileExt}`
+      const filePath = `${fileName}`
+
+      let { error: uploadError } = await supabase.storage
+        .from('files')
+        .upload(filePath, file)
+
+      if (uploadError) {
+        throw uploadError
+      }
+
+      onUpload(filePath)
+    } catch (error) {
+      alert(error.message)
+    } finally {
+      setUploading(false)
+    }
+  }
 
   return (
     <>
+      <div>
+        <label className="button primary block" htmlFor="single">
+          <MdUpload />
+        </label>
+        <input
+          style={{
+            visibility: 'hidden',
+            position: 'absolute',
+          }}
+          type="file"
+          id="single"
+          accept=""
+          onChange={uploadFile}
+          disabled={uploading}
+        />
+      </div>
       <input
         className="bg-base-100 border border-solid border-gray-300 appearance-none rounded w-full py-2 px-3 text-black-800 leading-tight focus:shadow-outline"
         type="text"

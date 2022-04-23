b23c45912afe7d05ca8c3c9ea65e95c44faa18fe
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-04-23 10:52:14 +0900

    Ver 0.6.0.2

--- components/NavBar.js
+++ components/NavBar.js
@@ -15,25 +15,27 @@ export default function NavBar({ channelname, sessionname, thispage }) {
               </a>
             </li>
             <li className="hidden sm:block">
-              <a>
-                @ {sessionname}　
-                <svg
-                  class="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                </svg>
-              </a>
-              <ul class="menu menu-horizontal p-2 bg-base-100">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
+              <ul class="menu menu-horizontal bg-base-100">
+                <a>
+                  @ {sessionname}　
+                  <svg
+                    class="fill-current"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+                  </svg>
+                </a>
+                <ul class="p-2 bg-base-100">
+                  <li>
+                    <a>Submenu 1</a>
+                  </li>
+                  <li>
+                    <a>Submenu 2</a>
+                  </li>
+                </ul>
               </ul>
             </li>
             <li>
